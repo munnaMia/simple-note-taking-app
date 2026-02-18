a70de1efb66fc03b20625e49880b2411a5aea95d
munnaMia <mdmunnamia987@gmail.com>
2026-02-18 12:31:07 +0600

    basic user untrusted input form to db

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,16 @@
 <?php
 
+$config = require('config.php');
+$db = new Database($config, 'munna', '3m@MJ#Sha4787mu');
+
+
 $headline = 'Create a new note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    dumpAndDie($_POST);
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body , :user_id);', [
+        'body' => $_POST['body'], // form body value
+        'user_id' => 3,
+    ]);
 }
 require 'views/note-create.view.php';
