7127b21b2f0f7b5799f99e0763fbf70c694c5428
munnaMia <mdmunnamia987@gmail.com>
2026-03-02 10:11:32 +0600

    basic middlware impl

--- Core/Router.php
+++ Core/Router.php
@@ -11,39 +11,68 @@ class Router
         $this->routes[] = [
             'uri' => $uri,
             'controller' => $controller,
-            'method' => $method
+            'method' => $method,
+            'middleware' => null  // by default every routes has a middleware which is nulll
         ];
+
+        return $this;
     }
 
     public function get($uri, $controller)
     {
-        $this->add('GET', $uri, $controller);
+        return $this->add('GET', $uri, $controller);
     }
 
     public function post($uri, $controller)
     {
-        $this->add('POST', $uri, $controller);
+        return $this->add('POST', $uri, $controller);
     }
 
     public function delete($uri, $controller)
     {
-        $this->add('DELETE', $uri, $controller);
+        return $this->add('DELETE', $uri, $controller);
     }
 
     public function patch($uri, $controller)
     {
-        $this->add('PATCH', $uri, $controller);
+        return $this->add('PATCH', $uri, $controller);
     }
 
     public function put($uri, $controller)
     {
-        $this->add('PUT', $uri, $controller);
+        return $this->add('PUT', $uri, $controller);
+    }
+
+    //middleware
+    public function only($key)
+    {
+        // here when this func will be called the current route is adding the only will be associate with that only
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+        return $this; // so we can chain further the middleware
     }
 
     public function route($uri, $method)
     {
         foreach ($this->routes as $route) {
             if ($route['uri'] == $uri && $route['method'] == strtoupper($method)) {
+                // here let's apply the middelware
+                if ($route['middleware'] == 'guest'){
+                    if ($_SESSION['user'] ?? false){
+                        header('location: /');
+                        exit;
+                    }
+                }
+
+                // here let's apply the middelware
+                if ($route['middleware'] == 'auth'){
+                    if (! $_SESSION['user'] ?? false){
+                        header('location: /');
+                        exit;
+                    }
+                }
+
+
                 require base_path($route['controller']);
                 exit();
             }
--- routes.php
+++ routes.php
@@ -4,7 +4,7 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 
 $router->get('/notes/create', 'controllers/notes/create.php'); // show the form
@@ -14,5 +14,5 @@ $router->delete('/note', 'controllers/notes/destroy.php');
 $router->get('/note/edit', 'controllers/notes/edit.php'); // show edit page
 $router->patch('/note', 'controllers/notes/update.php'); // update note
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
